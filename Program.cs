4f2b6c02ffceac103a02fe324d5d0a1f414cd725
Gabriel Wasiluk <17027711+gabwasik@users.noreply.github.com>
2024-12-01 22:37:29 +0100

    Implemented fetching and saving facts

--- Program.cs
+++ Program.cs
@@ -1,5 +1,18 @@
-﻿const string ApiUrl = "https://catfact.ninja/fact";
+﻿using CatFacts.Interfaces;
+using CatFacts.Models;
+using CatFacts.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+const string ApiUrl = "https://catfact.ninja/fact";
 const string FileName = "facts.txt";
+
+ServiceProvider serviceProvider = new ServiceCollection()
+    .AddHttpClient()
+    .AddSingleton<IFileService, FileService>()
+    .AddTransient<IResponseService<CatFact>, CatFactResponseService>()
+    .BuildServiceProvider();
+
+IResponseService<CatFact> responseService = serviceProvider.GetRequiredService<IResponseService<CatFact>>();
 
 while (true)
 {
@@ -15,5 +28,18 @@ while (true)
     if (input == "exit")
     {
         break;
+    }
+
+    if (input == "fact")
+    {
+        Console.WriteLine("Fetching a fact, please wait...");
+        CatFact? response = await responseService.FetchResponseAsync(ApiUrl);
+
+        if (response != null)
+        {
+            Console.Clear();
+            Console.WriteLine(response.Fact + Environment.NewLine);
+            responseService.SaveResponseToFile(FileName, response);
+        }
     }
 }
