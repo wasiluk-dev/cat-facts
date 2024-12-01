4cff767f0c6dfcc1acfbbda49e6249dfc6e2efbc
Gabriel Wasiluk <17027711+gabwasik@users.noreply.github.com>
2024-12-01 22:15:54 +0100

    Created a ResponseService for CatFact model

--- Models/CatFact.cs
+++ Models/CatFact.cs
@@ -1,7 +1,11 @@
-﻿namespace CatFacts.Models;
+﻿using System.Text.Json.Serialization;
+
+namespace CatFacts.Models;
 
 public class CatFact(string fact, int length)
 {
+    [JsonPropertyName("fact")]
     public string Fact { get; init; } = fact;
+    [JsonPropertyName("length")]
     public int Length { get; init; } = length;
 }
---
+++ Services/CatFactResponseService.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json;
+using CatFacts.Interfaces;
+using CatFacts.Models;
+
+namespace CatFacts.Services;
+
+public class CatFactResponseService(IHttpClientFactory httpClientFactory, IFileService fileService)
+    : IResponseService<CatFact>
+{
+    public async Task<CatFact?> FetchResponseAsync(string url)
+    {
+        HttpClient client = httpClientFactory.CreateClient();
+        HttpResponseMessage response = await client.GetAsync(url);
+        
+        response.EnsureSuccessStatusCode();
+        return JsonSerializer.Deserialize<CatFact>(await response.Content.ReadAsStringAsync());
+    }
+
+    public void SaveResponseToFile(string path, CatFact response)
+    {
+        fileService.AppendAllText(path, $"\"{response.Fact}\",{response.Length}");
+    }
+}
