4c8e6cc9b07c9c66c4a8cd6355374f14f9f7e738
Gabriel Wasiluk <17027711+gabwasik@users.noreply.github.com>
2024-12-01 21:24:21 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿const string ApiUrl = "https://catfact.ninja/fact";
+const string FileName = "facts.txt";
+
+while (true)
+{
+    Console.WriteLine("Available commands:" + Environment.NewLine +
+                      "– 'fact' to get a cool cat fact," + Environment.NewLine + 
+                      "– 'list' to see all the previously fetched facts," + Environment.NewLine + 
+                      "– 'clear' to delete all the stored facts," + Environment.NewLine + 
+                      "– 'exit' to exit the app.");
+    
+    string? input = Console.ReadLine()?.Trim().ToLower();
+    Console.Clear();
+    
+    if (input == "exit")
+    {
+        break;
+    }
+}
