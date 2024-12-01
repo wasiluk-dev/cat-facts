acd74197738f64c99c2c732c27a9f44cc4d662b6
Gabriel Wasiluk <17027711+gabwasik@users.noreply.github.com>
2024-12-01 23:03:09 +0100

    Added ReadResponsesFromFile method to IResponseService

--- Interfaces/IResponseService.cs
+++ Interfaces/IResponseService.cs
@@ -3,5 +3,6 @@
 public interface IResponseService<T>
 {
     Task<T?> FetchResponseAsync(string url);
+    List<T> ReadResponsesFromFile(string path);
     void SaveResponseToFile(string path, T response);
 }
